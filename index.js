6ea019a1289ece0e9f3a5f64f229f116754ec5eb
Samantha Meyer <slmeyer90@gmail.com>
2022-11-23 21:59:36 -0500

    dns resolve for url validation, reworked code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
-// const dns = require('dns');
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -12,19 +12,10 @@ app.use(cors());
 
 // Use body-parser to parse POST requests
 let bodyParser = require("body-parser");
-const { doesNotMatch } = require('assert');
-const { type } = require('os');
-const { url } = require('inspector');
 app.use(bodyParser.urlencoded({ extended: false } ));
 
 // Connect to mongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// // Set options for dns.lookup
-// const options = {
-//   family: 6,
-//   hints: dns.ADDRCONFIG | dns.V4MAPPED
-// };
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -45,52 +36,57 @@ const urlSchema = new Schema ({
 });
 let UrlShort = mongoose.model("UrlShort", urlSchema);
 
-// // Function to format URL for dns.lookup
-// const formatURL = url => {
-//   if (/^https:\/\//.test(url)) {
-//     return url.slice(8);
-//   }
-//   return url;
-// };
+// Function to format URL for dns.lookup
+const formatURL = url => {
+  if (/^https:\/\//.test(url)) {
+    return url.slice(8);
+  }
+  return url;
+};
 
-// Function to test if given url is valid
-const isValidUrl = url => {
-  try {
-    return Boolean(new URL(url));
-  }
-  catch(err) {
-    return false;
-  }
+// Function to test if input url is valid
+const isValidUrl = (url) => {
+
+  var test = dns.resolve(url, async (err) => {
+    if (err) {
+      return false
+    } else {
+      return true
+    }
+  })
 }
 
 // Get data from POST requests
+// Return original and short url or invalid url
 app.post('/api/shorturl', (req, res) => {
+  
   let originalUrl = req.body.url;
+  let formattedUrl = formatURL(originalUrl);
 
-  // Test url for validity, return error if invalid
-  if (isValidUrl(originalUrl) === false) {
-    res.json({ "error": "invalid url" })
-  } else {
-  // Search database for existing matching url
-  UrlShort
-    .findOne({
-      original_url: originalUrl
-    })
-    .then(data => {
-      if (data) {
-        res.json({
-          "original_url": data.original_url,
-          "short_url": data.short_url
-        });
-      } else {
-        UrlShort
-          .countDocuments()
+
+  dns.resolve(formattedUrl, (err, records) => {
+    if (err) {
+      res.json({"error": "invalid url"});      // if invalid, res error
+    } else {
+      UrlShort
+      .findOne({                              // if valid, search db for existing record
+        original_url: originalUrl
+      })
+      .then(data => {
+        if (data) {                           // if record exists, return data
+          res.json({
+            "original_url": data.original_url,
+            "short_url": data.short_url
+          });
+        } else {                              // else, create new record
+          UrlShort
+          .countDocuments()                   // generate short url
           .then(data => {
             let newUrl = new UrlShort({
               original_url: originalUrl,
               short_url: data
-            })
-            newUrl.save()
+            });
+            newUrl.save();
             res.json({
               "original_url": newUrl.original_url,
               "short_url": newUrl.short_url
@@ -98,63 +94,14 @@ app.post('/api/shorturl', (req, res) => {
           })
           .catch(err => {
             console.error(err);
-          })
-      };
-    })
-    .catch(err => {
-      console.error(err);
-    });
-  }
-
-
-  // Return existing record if match is found
-
-  // Create new record if no match is found
-
-
-  // let { url: originalUrl } = req.body;
-  // let formattedURL = formatURL(originalUrl);
-
-  // // validate url, send error if invalid
-  // dns.lookup(formattedURL, options, (err, address, family) => {
-  //   if (err) {
-  //     res.json({ "error": "invalid url" });
-  //     return;
-  //   };
-  //   // Search database and create new record if no matches are found
-  //   URL
-  //     .findOne({
-  //       url: originalUrl
-  //     })
-  //     .then(data => {
-  //       if (data) {
-  //         res.json({
-  //           "original_url": data.url,
-  //           "short_url": data.shortUrl
-  //         });
-  //       } else {
-  //         URL
-  //           .countDocuments()
-  //           .then(data => {
-  //             data++;
-  //             let newURL = new URL({ url: originalUrl, shortUrl: data });
-  //             newURL.save()
-  //               .then(data => {
-  //                 res.json({ 
-  //                   "original_url": data.url,
-  //                   "short_url": data.shortUrl
-  //                 });
-  //               })
-  //               .catch(err => {
-  //                 console.error(err)
-  //               });
-  //           });
-  //       }
-  //     })
-  //     .catch(err => {
-  //       console.error(err)
-  //     });
-  // });
+          });
+        }
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    };
+  });
 });
 
 // Redirect when user visits /api/shorturl/<short_url>
